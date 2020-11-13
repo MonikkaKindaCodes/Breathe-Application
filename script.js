691cc60a2b2fd5ae6a54e4490f9ae197d6d312ce
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-13 14:50:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+const container = documement.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+console.log(breatheTime, holdTime);
